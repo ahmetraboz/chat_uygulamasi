167210ba5166fae37f0d5f33583bf73ddbb33698
Ahmet Boz <aboz11897@gmail.com>
2024-07-29 17:23:23 +0300

    firebase auth service tamamlandı

--- lib/core/firebase_auth_service.dart
+++ lib/core/firebase_auth_service.dart
@@ -1,11 +1,53 @@
-//use firebase to connect server
-//login for auth
-
-
+import 'package:chat_uygulamasi/model/user_model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 
 class AuthService {
-  
-  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
-  
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  User? get currentUser => _firebaseAuth.currentUser;
+
+  Future<UserModel?> signUpWithEmailPassword(
+      String email, String password) async {
+    try {
+      UserCredential userCredential =
+          await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      if (userCredential.user == null) {
+        return null;
+      }
+      return UserModel(
+          name: userCredential.user!.displayName ?? "null",
+          email: userCredential.user!.email ?? "null");
+    } catch (e) {
+      debugPrint('Kayıt hatası: $e');
+      return null;
+    }
+  }
+
+  Future<User?> signInWithEmailPassword(String email, String password) async {
+    try {
+      UserCredential userCredential =
+          await _firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return userCredential.user;
+    } catch (e) {
+      debugPrint('error: $e');
+      return null;
+    }
+  }
+
+  Future<void> signOut() async {
+    try {
+      await _firebaseAuth.signOut();
+    } catch (e) {
+      debugPrint('ERROR $e');
+    }
+  }
+
+  Stream<User?> get authStateChanges => _firebaseAuth.authStateChanges();
 }
